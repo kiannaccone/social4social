8c2bc27c6fc24ed15b25d8f71a7a92ef187fad79
Katelyn <katelynjewel@gmail.com>
2021-12-05 14:25:27 -0800

    seeding data started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,98 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleteing previous data..."
+Comment.destroy_all
+User.destroy_all
+Post.destroy_all
+
+puts "🌱 Seeding..."
+puts "👤 Creating users..."
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+  User.create(
+    email: Faker::Internet.user('email'),
+    username: Faker::Internet.user('username'),
+    password_digest: Faker::Internet.user('password'),
+    bio: Faker::Lorem.paragraph(sentence_count: 2)
+    image: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+  )
+
+
+puts "🖥️ Creating posts..."
+50.times do
+  Post.create(
+    content: Faker::Twitter.status, 
+    like: rand(1-15), 
+    user_id: User.all.sample.id
+  )
+
+
+puts "📝 Creating comments..."
+40.times do 
+  AdoptionApplication.create(
+    body: Faker::Marketing.buzzwords,  
+    user_id: User.all.sample.id,  
+    post_id: Post.all.sample.id  
+  )
+end
+
+puts "✅ Done seeding!"
