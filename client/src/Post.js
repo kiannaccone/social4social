7ea0faeab6a00a6b20ea94fea328b8d793030542
garrettdick <garrettd0511@gmail.com>
2021-12-08 20:08:28 -0600

    Started to build the post form and have the skeleton layed out

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,14 +1,26 @@
-import PostContainer from "./PostContainer";
 
-function Post({allPost, currentUser}){
-    // console.log({currentUser})
+function Post(){
     
 
     return(
         <div>
-            <PostContainer allPost={allPost} currentUser={currentUser}/> 
+            <form>
+                <label>
+                    What's on your mind?
+                    <textarea onChange='' type='text' name='post' value=''>
+                    </textarea>
+                </label>
+            </form>
+            <button>Share Post</button>
         </div>
     )
 }
 
 export default Post;
+
+
+
+// We still need to:
+
+// - create the "handleChange" function as well as the "handleSubmit"
+// - we are going to need to use state to update the post and have it push back to the server 
